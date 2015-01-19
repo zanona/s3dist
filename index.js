0e54ac0160addc9d8f7da58077513ae258c58439
Marcus Zanona <zanona@me.com>
2015-01-19 16:21:09 +0000

    Add image files handling

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function main(d, v, flags, amazonBucket, deployDir) {
     deployDir = deployDir || 'build/';
     var s3 = require('s3'),
         Q = v.require('q'),
+        path = require('path'),
+        tmpDir,
         sizeMap = {},
         client = s3.createClient({
             s3Options: {
@@ -17,24 +19,31 @@ function main(d, v, flags, amazonBucket, deployDir) {
             deleteRemoved: true,
             s3Params: { Bucket: amazonBucket },
             getS3Params: function (localFile, stat, callback) {
-                var p = {
-                        ContentEncoding: 'gzip',
-                        Metadata: { 'raw-content-length': sizeMap[localFile] }
-                    };
-                if (localFile.match(/\.js|\.css/)) {
+                console.log('>', path.relative(tmpDir, localFile));
+                var p = {};
+                if (localFile.match(/\.(html|js|css)/)) {
+                    p.ContentEncoding = 'gzip';
+                    p.Metadata = { 'raw-content-length': sizeMap[localFile] };
+                }
+                if (localFile.match(/\.(html)/)) {
+                    p.CacheControl = 'no-cache';
+                }
+                if (localFile.match(/\.(js|css)/)) {
+                    p.CacheControl = 'max-age=31536000';
+                }
+                if (localFile.match(/\.(jpg|png|gif)/)) {
                     p.CacheControl = 'max-age=31536000';
                 }
                 callback(null, p);
             }
         },
-        tmpDir,
         uploader;
 
-    v.exec('mktemp -dt boilerplate').then(function (p) {
+    v.exec('mktemp -dt XXXXXX').then(function (p) {
         tmpDir = p.replace(/\n/, '');
-        return v.exec('cp ' + deployDir  + '* ' + p);
+        return v.exec('cp -r ' + deployDir  + '* ' + tmpDir);
     }).then(function () {
-        return Q.all(v.getFilteredFileList(tmpDir).map(function (f) {
+        return Q.all(v.getFilteredFileList(tmpDir, /\.(html|js|css)/).map(function (f) {
             return v.exec('wc -c <"' + f + '"')
                 .then(function (size) {
                     sizeMap[f] = size.replace(/\s/g, '');
@@ -51,12 +60,8 @@ function main(d, v, flags, amazonBucket, deployDir) {
         params.localDir = tmpDir;
         uploader = client.uploadDir(params);
         uploader.on('error', d.reject);
-        uploader.on('progress', function() {
-          //console.log('progress', uploader.progressAmount, uploader.progressTotal);
-        });
-        uploader.on('end', function() {
-          d.resolve('Done Uploading');
-        });
+        //on end being triggered before upload finishes
+        //uploader.on('end', function() { d.resolve('Done Uploading'); });
     });
 }
 
